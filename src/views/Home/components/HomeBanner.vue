bd1d9f656b7fa9876fcf1f23f8c39856392f8148
jmx <123@qq.com>
2025-10-28 19:56:23 +0800

    轮播图

--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,5 +1,13 @@
-<!-- <script setup>
+<script setup>
+import { getBannerAPI } from '@/apis/home';
+import { onMounted, ref } from 'vue';
+const bannerList = ref([])
+const getBanner = async () => {
+  const res = await getBannerAPI()
+  bannerList.value = res.data.result
+}
 
+onMounted(() => getBanner())
 </script>
 
 
@@ -7,8 +15,8 @@
 <template>
   <div class="home-banner">
     <el-carousel height="500px">
-      <el-carousel-item v-for="item in 4" :key="item">
-        <img src="http://yjy-xiaotuxian-dev.oss-cn-beijing.aliyuncs.com/picture/2021-04-15/6d202d8e-bb47-4f92-9523-f32ab65754f4.jpg" alt="">
+      <el-carousel-item v-for="item in bannerList" :key="item.id">
+        <img :src="item.imgUrl" alt="">
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -30,7 +38,4 @@
     height: 500px;
   }
 }
-</style> -->
-<template>
-  11
-</template>
+</style>
